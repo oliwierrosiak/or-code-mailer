35728435144a3818858443f1b9a4c0ea7d1c424f
Oliwier <oliwierrosiak07@gmail.com>
2026-02-16 11:17:25 +0100

    Added server wake up

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,10 @@ import ApiController from '../controllers/api.js'
 
 const router = new Router()
 
+router.get('/serverStart',(req,res)=>{
+    res.sendStatus(200)
+})
+
 router.post('/api/send',ApiController.sendEmail)
 
 export default router
